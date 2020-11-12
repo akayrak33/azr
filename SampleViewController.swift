0d377fc917e63281fb4d13ee55628c1c4257c924
Alaattin KAYRAK <akayrak33@gmail.com>
2020-11-12 18:03:20 +0300

    Add files via upload

---
+++ SampleViewController.swift
@@ -0,0 +1,51 @@
+import UIKit
+import RxSwift
+
+class SampleViewController: UIViewController {
+    let service = BaseService()
+    let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        request(onSuccess: { response in
+            print(response.url)
+        }, onError: {error in
+            print(error.errorMessage, error.errorCode)
+        })?.disposed(by: disposeBag)
+    }
+
+    func request(onSuccess: @escaping (GetRequestUIModel) -> Void,
+                 onError: @escaping (Error) -> Void) -> Disposable? {
+
+        return nil
+    }
+
+    func call<T, U>(withMapper mapper: @escaping (T) -> (U?),
+                    observable: Observable<T>,
+                    onSuccess: @escaping (U) -> Void,
+                    onError: @escaping (Error) -> Void) -> Disposable? {
+
+        return nil
+    }
+}
+
+class GetRequestResponse: Decodable {
+    let url: String
+}
+
+class GetRequestUIModel {
+    let url: String
+
+    init(url: String) {
+        self.url = url
+    }
+}
+
+class GetRequestUIModelConverter {
+    static func convert(with response: Result<GetRequestResponse>) -> GetRequestUIModel? {
+        guard let data = response.response else { return nil }
+
+        return GetRequestUIModel(url: data.url)
+    }
+}
